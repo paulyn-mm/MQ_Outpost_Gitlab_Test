b191ab360308f02706f126eef1d93e8a4fac23a1
paulynpelayo <paulyn.ana.pelayo@mediamonks.com>
2021-12-13 15:34:52 +0800

    initial test scripts

---
+++ MQ_Outpost_Gitlab/Assets/Editor/SomeTest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class SomeTest
+{
+    [Test]
+    public void SomeTestPassing()
+    {
+        Assert.Pass();
+    }
+}
